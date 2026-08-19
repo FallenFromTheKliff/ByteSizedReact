922d0581d85ed7f84fc5414b39879688abcf9203
ewankoadduno-cmd <tontondeleon69@gmail.com>
2026-08-20 03:58:53 +0800

    Review

--- src/pages/ElectricityBill.jsx
+++ src/pages/ElectricityBill.jsx
@@ -1,11 +1,39 @@
+import { useState,  } from "react"
+
 export default function ElectricityBill() {
+  const [customerName, setCustomerName] = useState('');
+  const [pageState, SetPageState] = useState("");
+
+  function Clear(){}
+  function Submit(){}
+  
+
+
   return (
     <div className="flex flex-1 flex-col w-full items-center bg-indigo-50">
-      <h1 className="text-3xl font-bold mt-12">I blame Meralco</h1>
+      <h1 className="">Electricity Bill Calculator {customerName}</h1>
+
+      <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-xl p-8 text-white">
+        <input type="text" 
+          placeholder="Enter Customer Name"
+          value={customerName}
+          onChange={(event) => {setCustomerName(event.target.value)}}
+          />
 
 
-    
-      <div>HEY BRO </div>
+          <button type="button" onMouseEnter={Clear}>Clear</button>
+          <button type="button">Submit</button>
+
+
+      
+      
+      <div>Sample Form {name}</div>
+      
+
+      </div>
+
+
+
     </div>
   )
 }
